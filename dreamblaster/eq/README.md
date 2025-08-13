3fb1c7aa7661124940bde96afc0f5a8e5c00ce5d
Andy (PS i have no idea how GitHub works) <andrew.p.rea@gmail.com>
2025-08-13 14:39:55 +0100

    Delete dreamblaster directory
    not part of mod, see original work for other parts

--- dreamblaster/eq/README.md
+++
@@ -1,2 +0,0 @@
-# mogg-e-q
-E.Q. explorer for the s2 Dreamblaster as driven by a Sinclair ZX81  because I can.
--- dreamblaster/eq/build.bat
+++
@@ -1,9 +0,0 @@
-@set EO_SD=C:\Users\Developer\Desktop\retro\EightyOneV1.8\ZXpand_SD_Card
-
-set src=_-_eq.asm
-set dest=eq.p
-
-brass %src% %dest% -s -l listing.html
-
-@copy %dest% %EO_SD%\menu.p
-@copy %dest%.sym %EO_SD%\menu.p.sym
